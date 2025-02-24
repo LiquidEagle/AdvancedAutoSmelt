876aa9d562ae0714e90acd8d40b8182605223099
Pulsi_ <fastellialessio@gmail.com>
2025-02-24 09:37:34 +0100

    Added autosell multipliers. Optimized code and minor fixes. Added autosell and autosell_multiplier placeholders.

--- src/main/java/me/pulsi_/advancedautosmelt/placeholders/Placeholders.java
+++ src/main/java/me/pulsi_/advancedautosmelt/placeholders/Placeholders.java
@@ -2,6 +2,9 @@ package me.pulsi_.advancedautosmelt.placeholders;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.pulsi_.advancedautosmelt.AdvancedAutoSmelt;
+import me.pulsi_.advancedautosmelt.coreSystem.AutoSell;
+import me.pulsi_.advancedautosmelt.players.AASPlayer;
+import me.pulsi_.advancedautosmelt.players.PlayerRegistry;
 import me.pulsi_.advancedautosmelt.values.ConfigValues;
 import org.bukkit.entity.Player;
 
@@ -36,18 +39,26 @@ public class Placeholders extends PlaceholderExpansion {
     public String onPlaceholderRequest(Player p, String identifier) {
         if (p == null) return "Player not online";
 
+        AASPlayer player = PlayerRegistry.getPlayer(p);
         switch (identifier) {
             case "autopickup":
-                if (true) return ConfigValues.getEnabledPlaceholder();
+                if (player != null && player.isAutoPickupEnabled()) return ConfigValues.getEnabledPlaceholder();
                 return ConfigValues.getDisabledPlaceholder();
 
             case "autosmelt":
-                if (true) return ConfigValues.getEnabledPlaceholder();
+                if (player != null && player.isAutoSmeltEnabled()) return ConfigValues.getEnabledPlaceholder();
+                return ConfigValues.getDisabledPlaceholder();
+
+            case "autosell":
+                if (player != null && player.isAutoSellEnabled()) return ConfigValues.getEnabledPlaceholder();
                 return ConfigValues.getDisabledPlaceholder();
 
             case "inventoryalerts":
-                if (true) return ConfigValues.getEnabledPlaceholder();
+                if (player != null && player.isInventoryAlertsEnabled()) return ConfigValues.getEnabledPlaceholder();
                 return ConfigValues.getDisabledPlaceholder();
+
+            case "autosell_multiplier":
+                return AutoSell.getAutoSellMultiplier(p) + "";
         }
         return null;
     }
